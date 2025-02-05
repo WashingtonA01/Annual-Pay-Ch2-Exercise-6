af2b145e43d3eeba63d4cb7dd15e04d047dd955a
WashingtonA01 <arianna.s.washington@gmail.com>
2025-02-05 08:12:04 -0500

    Master Branch

--- Annual Pay Ch2 Exercise 6.cpp
+++ Annual Pay Ch2 Exercise 6.cpp
@@ -3,18 +3,18 @@
 
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-    std::cout << "Hello World!\n";
+	float payAmount = 2200.0;
+	int payPeriods = 26;
+	float annualPay = payAmount * payPeriods;
+
+	cout << "Paycheck amount every two weeks:" << " " << payAmount << endl;
+
+	cout << "Pay periods per year:" << " " << payPeriods << endl;
+
+	cout << "Total annual pay:" << " " << annualPay;
+
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
